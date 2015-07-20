26d6b6ef4d640c989447f4f390e34cb7125384e7
Jared Bienz [MSFT] <jbienzms@hotmail.com>
2015-07-20 15:17:35 -0500

    First commit to source control. Basic code generation using Roslyn is now up and running. Basic front-end GUI tool working to generate and view code.

---
+++ GUI/IoTTools/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.IoTTools;
+
+namespace IoTTools
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SketchConverter converter;
+        public MainWindow()
+        {
+            InitializeComponent();
+            converter = new SketchConverter();
+        }
+
+        private void GoButton_Click(object sender, RoutedEventArgs e)
+        {
+            OutputBox.Text = converter.Convert();
+        }
+    }
+}
---
+++ Lib/Microsoft.IoTTools/SketchConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.MSBuild;
+using SF = Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace Microsoft.IoTTools
+{
+    public class SketchConverter
+    {
+        public string Convert()
+        {
+            // Create root compilation unit and add some using statements
+            var cu = SF.CompilationUnit()
+                .AddUsings(SF.UsingDirective(SF.IdentifierName("System")))
+                .AddUsings(SF.UsingDirective(SF.IdentifierName("System.Generic")));
+
+            // Add the root namespace
+            var ns = SF.NamespaceDeclaration(SF.IdentifierName("Microsoft.IoT"));
+
+            // Create a class and make it public
+            var c = SF.ClassDeclaration("MyClass")
+                .AddModifiers(SF.Token(SyntaxKind.PublicKeyword));
+
+            // Add the class to the namespace
+            ns = ns.AddMembers(c);
+
+            // Add the namespace to the compilation unit
+            cu = cu.AddMembers(ns);
+
+            // Inject whitespace formatting using default C# rules
+            cu = cu.NormalizeWhitespace();
+
+            // Convert to string
+            return cu.ToFullString();
+        }
+    }
+}
